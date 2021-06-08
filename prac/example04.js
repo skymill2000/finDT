81dc7790e236a8b4d856d84307693a23e2671e7a
codestella <howtowhy@snu.ac.kr>
2021-06-08 22:20:19 +0900

    배열 실습

---
+++ prac/example04.js
@@ -0,0 +1,4 @@
+let array = ["bmw", "sonata", "honda"];
+console.log(array[0]);
+console.log(array[2]);
+
---
+++ prac/example05.js
@@ -0,0 +1,38 @@
+let car = {
+    name : "bmw",
+    ph : 500,
+    start : function(){
+        console.log("engine is started")
+    },
+    stop : function(){
+        console.log("engine is stoped")
+    }
+}
+
+let car2 = {
+    name : "sonata",
+    ph : 140,
+    start : function(){
+        console.log("engine is started")
+    },
+    stop : function(){
+        console.log("engine is stoped")
+    }
+}
+
+let car3 = {
+    name : "honda",
+    ph : 320,
+    start : function(){
+        console.log("engine is started")
+    },
+    stop : function(){
+        console.log("engine is stoped")
+    }
+}
+
+let cars = [car, car2, car3];
+console.log(cars[0].name);
+console.log(cars[1].name);
+console.log(cars[2].name);
+console.log(cars[1].ph);
