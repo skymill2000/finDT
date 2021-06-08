9eb266a10d7b8edb5936497151347b42840a237b
codestella <howtowhy@snu.ac.kr>
2021-06-08 21:30:17 +0900

    변수 선언 연습 let / const 사용 const  상수이기 때문에 재할당이 불가능 합니다.

---
+++ prac/example01.js
@@ -0,0 +1,13 @@
+let name = "홍길동";
+//Java Data type : Int String, Float, Double
+//int age = "";
+let age = 16;
+name = "고길동";
+let obj = {
+    name : "권기혁",
+    gender : "m"
+}
+
+
+console.log(name, age,obj);
+//특정항목을 터미널에 출력합니다.
