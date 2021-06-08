67b946b350b5c2bdd7b94c06ee10ff7754eb15ec
codestella <howtowhy@snu.ac.kr>
2021-06-08 22:12:39 +0900

    기능 완성

--- prac/example03.js
+++ prac/example03.js
@@ -6,11 +6,20 @@ const minus = (p1, p2) => {
     return p1 - p2;
 }
 
+const mul = (p1, p2) => {
+    return p1 * p2;
+}
+
+const div = (p1, p2) => {
+    return p1 / p2;
+}
+
+
 //minus, mul, div 의 이름을 가진
 //마이너스기능 , 곱셈, 나누기 기능을 추가해주세요
 
 
 console.log(plus(5, 6));
-console.log(plus(5, 8));
-console.log(plus(5, 800));
-
+console.log(minus(5, 8));
+console.log(div(5, 800));
+console.log(mul(5,3))
