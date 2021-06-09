bb3216cc531435380add579b3d772749aac66f52
DESKTOP-8C6KMVH\user <skymill2000>
2021-06-09 20:58:10 +0900

    리액트 라우터 페이지 추가 / test / list

--- fintech/src/App.js
+++ fintech/src/App.js
@@ -1,17 +1,21 @@
 import logo from './logo.svg';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import FirstExample from './components/FirstExample';
+import UserList from './components/UserList';
 
 
 function App() {
-  return     
+  return (    
   <Router>
-  <Switch>
-    <Route path="/axios" exact>
-
-    </Route>
+    <Switch>
+      <Route path="/test" exact>
+        <FirstExample></FirstExample>
+      </Route>
+      <Route path="/list" exact>
+        <UserList></UserList>
+      </Route>
   </Switch>
-</Router>
-
+</Router>)
 }
 
 export default App;
