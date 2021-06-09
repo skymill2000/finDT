3c070e715504c105e22d1e48c9899df82bf7a1f2
DESKTOP-8C6KMVH\user <skymill2000>
2021-06-09 20:16:06 +0900

    첫 실습 코드 컴포넌트화

--- fintech/src/App.js
+++ fintech/src/App.js
@@ -1,27 +1,8 @@
+import FirstExample from './components/FirstExample';
 import logo from './logo.svg';
-import {useState} from 'react';
-import './App.css';
-
-const Welcome = ({username, age}) => {
-  return (<h1>안녕하세요 {username}(이름) {age}(세) 님</h1>);
-}
-
 
 function App() {
-  const [username, setUsername] = useState("입력된 이름이 없습니다.");
-  const changeName = (event) =>{
-    console.log(event.target.value);
-    setUsername(event.target.value);
-  }
-
-  return (
-    <div className="App">
-      <header className="App-header">
-        <Welcome username={username} age="33"></Welcome>
-        <input onChange={changeName}></input>
-      </header>
-    </div>
-  );
+  return<FirstExample></FirstExample>
 }
 
 export default App;
---
+++ fintech/src/components/FirstExample.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import {useState} from 'react';
+import '../App.css';
+
+const Welcome = ({username, age}) => {
+    return (<h1>안녕하세요 {username}(이름) {age}(세) 님</h1>);
+  }
+  
+const FirstExample = () => {
+    const [username, setUsername] = useState("입력된 이름이 없습니다.");
+    const changeName = (event) =>{
+      console.log(event.target.value);
+      setUsername(event.target.value);
+    }
+  
+    return (
+      <div className="App">
+        <header className="App-header">
+          <Welcome username={username} age="33"></Welcome>
+          <input onChange={changeName} ></input>
+        </header>
+      </div>
+    );
+}
+
+export default FirstExample
