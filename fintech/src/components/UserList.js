77c314911c9ff591c3f096c690231d5eec12ea7b
DESKTOP-8C6KMVH\user <skymill2000>
2021-06-09 20:50:25 +0900

    STYLED COMPONENT 추가

--- fintech/src/components/UserList.js
+++ fintech/src/components/UserList.js
@@ -1,4 +1,21 @@
 import React, {useState} from 'react'
+import styled from "styled-components";
+
+const Title = styled.div`
+    border-radius: 3px;
+    padding: 0.5rem 0;
+    margin: 0.5rem 1rem;
+    width: 11rem;
+    background: transparent;
+    color: red;
+    border: 2px solid white;
+`
+
+const MyButton = styled.button`
+    border : solid 1px #ff0000;
+    padding : 30px;
+    background : blue;
+`
 
 
 const UserList = () => {
@@ -19,8 +36,9 @@ const UserList = () => {
     return (
         <div>
             {carList.map(car => {
-                return(<p>자동차 이름은 {car.name}</p>)
+                return(<Title>자동차 이름은 {car.name}</Title>)
             })}
+            <MyButton>버튼입니다</MyButton>
         </div>
     )
 }
