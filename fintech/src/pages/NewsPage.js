3d2a36c399eb192b6ecd2677c1b990a3da1f23aa
DESKTOP-8C6KMVH\user <skymill2000>
2021-06-09 22:40:04 +0900

    검색어 입력 기능 추가

--- fintech/src/pages/NewsPage.js
+++ fintech/src/pages/NewsPage.js
@@ -3,19 +3,25 @@ import axios from 'axios'
 
 const NewsPage = () => {
     const [newsList, setNewsList] = useState([])
+    const [searchInput, setSearchInput] = useState('')
     const getNews = () =>{
         //버튼을 클릭한 이후에 뉴스데이터를 가져오는것
         // axios.get("{주소}")
         let apiKey = "78bc6ddd8cdb48ceac76f5f9b9dfc4c5" //여러분들의 APIkEY
-        let newsApiUrl = "https://newsapi.org/v2/everything?q=삼성&from=2021-05-09&sortBy=publishedAt&apiKey="+apiKey+"&language=ko";
+        let newsApiUrl = "https://newsapi.org/v2/everything?q="+searchInput+"&from=2021-05-09&sortBy=publishedAt&apiKey="+apiKey+"&language=ko";
         axios.get(newsApiUrl).then((response)=>{
             let resultNewsList = response.data.articles;
             setNewsList(resultNewsList)
             console.log(newsList);
         })
     }
+    const changeSearch = (e) =>{
+        const {value, name} = e.target;
+        setSearchInput(value);
+    }
     return (
         <div>
+            <input onChange={changeSearch}></input>
             <button onClick={getNews}>뉴스 가져오기</button>
             {newsList.map( (news) =>{
                 return(
