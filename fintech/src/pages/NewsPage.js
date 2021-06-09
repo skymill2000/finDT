c160a737d1fe56270837c4f61be28f3efdb9b5ee
DESKTOP-8C6KMVH\user <skymill2000>
2021-06-09 22:36:14 +0900

    news API 완성

--- fintech/src/pages/NewsPage.js
+++ fintech/src/pages/NewsPage.js
@@ -1,21 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 import axios from 'axios'
 
 const NewsPage = () => {
+    const [newsList, setNewsList] = useState([])
     const getNews = () =>{
         //버튼을 클릭한 이후에 뉴스데이터를 가져오는것
         // axios.get("{주소}")
-        let apiKey = "" //여러분들의 APIkEY
+        let apiKey = "78bc6ddd8cdb48ceac76f5f9b9dfc4c5" //여러분들의 APIkEY
         let newsApiUrl = "https://newsapi.org/v2/everything?q=삼성&from=2021-05-09&sortBy=publishedAt&apiKey="+apiKey+"&language=ko";
         axios.get(newsApiUrl).then((response)=>{
-            console.log(response)
+            let resultNewsList = response.data.articles;
+            setNewsList(resultNewsList)
+            console.log(newsList);
         })
     }
-
-
     return (
         <div>
-            <button onClick={getNews}>뉴스 가져오기</button>            
+            <button onClick={getNews}>뉴스 가져오기</button>
+            {newsList.map( (news) =>{
+                return(
+                <>
+                    <h1>{news.title}</h1>
+                    <p>{news.description}</p>
+                </>
+                )
+            })}            
         </div>
     )
 }
