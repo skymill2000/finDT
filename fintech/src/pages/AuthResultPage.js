64fa9daf1653bc293bc77f7b17be81b82d0ae128
DESKTOP-8C6KMVH\user <skymill2000>
2021-06-11 19:27:13 +0900

    axios를 통한 토큰 발급 요청 실시

--- fintech/src/pages/AuthResultPage.js
+++ fintech/src/pages/AuthResultPage.js
@@ -8,6 +8,8 @@ import qs from "qs";
 
 const AuthResultPage = () => {
   const { search } = useLocation();
+  //code=OlRKFtAPQW10JFPTE5jh2Otu7I0mxr&scope=inquiry%20login%20transfer&state=12345678901234567890123456789012
+  //=> javascrip object 변경
   const { code } = queryString.parse(search);
   const [accessToken, setaccessToken] = useState("토큰 받아오기 전 데이터");
 
@@ -16,7 +18,26 @@ const AuthResultPage = () => {
   }, []);
 
   const getAccessToken = () => {
+    const sendData = {
+        code : code,
+        client_id : "q7kH44ThJwjpvNRg0BbJvE1yxvx5X53DKz1rNgPF",
+        client_secret : "yVT6irMr2h4ZTHzZY7sDpbvhm1nlOzr4nP7DYRVy",
+        redirect_uri : "http://localhost:3000/authResult",
+        grant_type : "authorization_code"
+    }
 
+    const option = {
+        method : "POST",
+        url : "https://testapi.openbanking.or.kr/oauth/2.0/token",
+        headers :{
+            "Content-Type": "application/x-www-form-urlencoded",
+        },
+        data : sendData
+    }
+
+    axios(option).then(response => {
+        console.log(response.data);
+    })
   };
 
   return (
